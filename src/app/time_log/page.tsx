6447355760270d1a4d7a6f4233b2fa30108d97b3
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-05-15 18:25:18 -0300

    added page skeleton 👍

--- src/app/time_log/page.tsx
+++ src/app/time_log/page.tsx
@@ -8,23 +8,55 @@ export default function TimeLog() {
           <div className={styles.content}>
             <h1>IMG</h1>
             <div>
-              <p>LUCAS FERNANDES (UX/UI)</p>
-              <p>1 ANO / DESIGN</p>
+              <h2>LUCAS FERNANDES (UX/UI)</h2>
+              <h3>1º ANO / DESIGN</h3>
               <p>tarefas em andamento</p>
             </div>
-            <button>editar</button>
+            <button>Editar</button>
           </div>
           <div className={styles.hours}>
-            <p>Horas totais</p>
+            <h2>Horas totais</h2>
             <span>00:00</span>
           </div>
         </header>
-        <section className={styles.projects}>
-          <aside>
-            <h2>Projetos</h2>
-            <h2>Histórico</h2>
+
+        <section className={styles.body}>
+          <aside className={styles.widgets}>
+            <div>
+              <h2>Projetos</h2>
+              <button>\/</button>
+            </div>
+            <div>
+              <h2>Histórico</h2>
+              <button>\/</button>
+            </div>
           </aside>
-          <article>Projeto 1</article>
+          <article className={styles.container}>
+            <div>
+              <h2>PROJETO 1</h2>
+              <p>Início: 15/05/2023</p>
+              <p>PO / Scrum Master</p>
+              <button>Ver membros</button>
+            </div>
+            <div className={styles.project}>
+              <div className={styles.activities}>
+                <h3>ATIVIDADES</h3>
+                <span className={styles["activities-status"]}>Pendentes</span>
+                <div>
+                  <h4>TÍTULO</h4>
+                  <p>Área da atividade</p>
+                  <span>Início 28/10/2023</span>
+                  <button>administrar</button>
+                </div>
+                <div>
+                  <h4>TÍTULO</h4>
+                  <p>Área da atividade</p>
+                  <span>Início 28/10/2023</span>
+                  <button>administrar</button>
+                </div>
+              </div>
+            </div>
+          </article>
         </section>
       </main>
     </>
