1bda7a568271ac862b97dfc3aebc4a8f860889f1
Bruno Fevereiro <br.fevereiro@terra.com.br>
2023-05-23 13:49:49 -0300

    Project entity validations

--- src/@clean/shared/domain/entities/project.ts
+++ src/@clean/shared/domain/entities/project.ts
@@ -12,3 +12,45 @@ export type JsonProps = {
     description: string;
 }
 
+export class Project {
+    constructor (public props: ProjectProps) {
+
+    }
+
+    // Validations
+
+    static validateCode(code: string): boolean {
+        if (code == null) {
+            return false
+        } else if (typeof(code) != "string") {
+            return false
+        } else if (code.length != 2) {
+            return false
+        } else if (code == code.toUpperCase()) {
+            return false
+        } else if (!code.match(/^[A-Za-z]+$/)) {
+            return false
+        }
+        return true
+    }
+
+    static validateName(name: string): boolean {
+        if (name == null) {
+            return false
+        } else if (typeof(name) != "string") {
+            return false
+        } else if (name.length < 3) {
+            return false
+        }
+        return true
+    }
+
+    static validateDescription(description: string): boolean {
+        if (description == null) {
+            return false
+        } else if (typeof(description) != "string") {
+            return false
+        }
+        return true
+    }
+}
