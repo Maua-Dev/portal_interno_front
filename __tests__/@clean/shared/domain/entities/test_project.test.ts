5527f56d18db4e53825ad02b71c0d6cecc7398a0
Bruno Fevereiro <br.fevereiro@terra.com.br>
2023-05-23 15:02:34 -0300

    props tests

--- __tests__/@clean/shared/domain/entities/test_project.test.ts
+++ __tests__/@clean/shared/domain/entities/test_project.test.ts
@@ -1,6 +1,5 @@
 import { Project } from "@/@clean/shared/domain/entities/project"
 
-
 test('Test Project entity', () => {
     const project = new Project({
         code: 'PI', 
@@ -9,3 +8,33 @@ test('Test Project entity', () => {
     })
     expect(project).toBeInstanceOf(Project)
 });
+
+// Properties Tests
+
+test('Test Project entity code', () => {
+    const project = new Project({
+        code: 'PI', 
+        name: 'Portal Interno',
+        description: 'Site controle de membros', 
+    })
+    expect(project.code).toBe('PI')
+}) 
+
+test('Test Project entity name', () => {
+    const project = new Project({
+        code: 'PI', 
+        name: 'Portal Interno',
+        description: 'Site controle de membros', 
+    })
+    expect(project.name).toBe('Portal Interno')
+}) 
+
+test('Test Project entity description', () => {
+    const project = new Project({
+        code: 'PI', 
+        name: 'Portal Interno',
+        description: 'Site controle de membros', 
+    })
+    expect(project.description).toBe('Site controle de membros')
+}) 
+
