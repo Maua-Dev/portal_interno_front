46029f4ce90aac81b5fd7c984e50b0d300de3d04
Bruno Fevereiro <br.fevereiro@terra.com.br>
2023-05-23 13:30:57 -0300

    creating project entity

---
+++ src/@clean/shared/domain/entities/project.ts
@@ -0,0 +1,14 @@
+
+
+export type ProjectProps = {
+    code: string;
+    name: string;
+    description: string;
+}
+
+export type JsonProps = {
+    code: string;
+    name: string;
+    description: string;
+}
+
