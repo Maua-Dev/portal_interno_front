ff8b0382a096c635691a2d39d8981f1368d8ca70
Bruno Fevereiro <br.fevereiro@terra.com.br>
2023-05-24 14:38:35 -0300

    Invalid attributes test

--- __tests__/@clean/shared/domain/entities/test_associated_action.test.ts
+++ __tests__/@clean/shared/domain/entities/test_associated_action.test.ts
@@ -2,10 +2,11 @@ import { Action } from "@/@clean/shared/domain/entities/action";
 import { AssociatedAction } from "@/@clean/shared/domain/entities/associated_action";
 import { ACTION_TYPE } from "@/@clean/shared/domain/enums/action_type_enum";
 import { STACK } from "@/@clean/shared/domain/enums/stack_enum";
+import { EntityError } from "@/@clean/shared/domain/helpers/errors/domain_error";
 
 // Instance Test
 
-test('Test Associated Action entity code', () => {
+test('Test Associated Action entity', () => {
     const action = new Action({
         owner_ra: "21.00210-0",
         start_time: 1000,
@@ -119,4 +120,34 @@ test('Test Associated Action entity from JSON', () => {
     })
 
     expect(AssociatedAction.fromJSON(associatedAction)).toBeInstanceOf(AssociatedAction)
+})
+
+// Invalid attributes tests
+
+test('Test Associated Action entity error member_ra', () => {
+    const action = new Action({
+        owner_ra: "21.00210-0",
+        start_time: 1000,
+        end_time: 2000,
+        action_id: "4000",
+        title: "Test Action Entity",
+        project_code: "76",
+        associated_members_ra: ["21.00833-7"],
+        stack_tags: [STACK.FRONTEND, STACK.BACKEND],
+        action_type_tags: [ACTION_TYPE.CODE, ACTION_TYPE.LEARN],
+      });
+    
+      expect(() => {
+        new AssociatedAction({
+            member_ra: "20021940",
+            action: action,
+        })
+      }).toThrowError(EntityError)
+
+      expect(() => {
+        new AssociatedAction({
+            member_ra: "20021940",
+            action: action,
+        })
+      }).toThrowError('Field props.member_ra is not valid')
 })
