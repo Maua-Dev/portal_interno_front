dabf7dbee6d3cab299637d2e1dd090e39f2ed3a1
Bruno Fevereiro <br.fevereiro@terra.com.br>
2023-05-24 14:26:53 -0300

    From/To JSON tests

--- __tests__/@clean/shared/domain/entities/test_associated_action.test.ts
+++ __tests__/@clean/shared/domain/entities/test_associated_action.test.ts
@@ -5,7 +5,7 @@ import { STACK } from "@/@clean/shared/domain/enums/stack_enum";
 
 // Instance Test
 
-test('Test Associated Action Entity', () => {
+test('Test Associated Action entity code', () => {
     const action = new Action({
         owner_ra: "21.00210-0",
         start_time: 1000,
@@ -27,7 +27,7 @@ test('Test Associated Action Entity', () => {
 
 // Properties Tests
 
-test('Test Associated Action member_ra', () => {
+test('Test Associated Action entity member_ra', () => {
     const action = new Action({
         owner_ra: "21.00210-0",
         start_time: 1000,
@@ -47,7 +47,7 @@ test('Test Associated Action member_ra', () => {
     expect(associatedAction.member_ra).toBe('20.02194-0')
 })
 
-test('Test Associated Action action', () => {
+test('Test Associated Action entity action', () => {
     const action = new Action({
         owner_ra: "21.00210-0",
         start_time: 1000,
@@ -78,3 +78,45 @@ test('Test Associated Action action', () => {
       }))
 })
 
+// From JSON and to JSON tests 
+
+test('Test Associated Action entity to JSON', () => {
+    const action = new Action({
+        owner_ra: "21.00210-0",
+        start_time: 1000,
+        end_time: 2000,
+        action_id: "4000",
+        title: "Test Action Entity",
+        project_code: "76",
+        associated_members_ra: ["21.00833-7"],
+        stack_tags: [STACK.FRONTEND, STACK.BACKEND],
+        action_type_tags: [ACTION_TYPE.CODE, ACTION_TYPE.LEARN],
+      });
+    const associatedAction = new AssociatedAction({
+        member_ra: "20.02194-0",
+        action: action,
+    })
+
+    expect(associatedAction.toJSON()).toBeInstanceOf(Object)
+})
+
+
+test('Test Associated Action entity from JSON', () => {
+    const action = new Action({
+        owner_ra: "21.00210-0",
+        start_time: 1000,
+        end_time: 2000,
+        action_id: "4000",
+        title: "Test Action Entity",
+        project_code: "76",
+        associated_members_ra: ["21.00833-7"],
+        stack_tags: [STACK.FRONTEND, STACK.BACKEND],
+        action_type_tags: [ACTION_TYPE.CODE, ACTION_TYPE.LEARN],
+      });
+    const associatedAction = new AssociatedAction({
+        member_ra: "20.02194-0",
+        action: action,
+    })
+
+    expect(AssociatedAction.fromJSON(associatedAction)).toBeInstanceOf(AssociatedAction)
+})
