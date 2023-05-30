04c373685ff7372bfd5f30efd3031293a39f0da9
Bruno Fevereiro <br.fevereiro@terra.com.br>
2023-05-29 22:40:51 -0300

    Eslint Changes Done

--- __tests__/@clean/modules/usecases/test_update_user_usecase.test.ts
+++ __tests__/@clean/modules/usecases/test_update_user_usecase.test.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { UpdateUserUsecase } from '@/@clean/modules/user/usecases/update_user_usecase'
 import { User } from '@/@clean/shared/domain/entities/user'
 import { UserRepositoryMock } from '@/@clean/shared/infra/repositories/user_repository_mock'
@@ -12,18 +11,3 @@ test('Test update user usecase', async () => {
   expect(userUpdated).toBeInstanceOf(User)
   expect(userUpdated.name).toBe('Test')
 })
-=======
-import { UpdateUserUsecase } from '@/@clean/modules/user/usecases/update_user_usecase'
-import { User } from '@/@clean/shared/domain/entities/user'
-import { UserRepositoryMock } from '@/@clean/shared/infra/repositories/user_repository_mock'
-
-test('Test update user usecase', async () => {
-  const repo = new UserRepositoryMock()
-  const usecase = new UpdateUserUsecase(repo)
-
-  const userUpdated = await usecase.execute(1, 'Test')
-
-  expect(userUpdated).toBeInstanceOf(User)
-  expect(userUpdated.name).toBe('Test')
-})
->>>>>>> 325e980e6943443e87c493f123ddd5fe821a6e3b
