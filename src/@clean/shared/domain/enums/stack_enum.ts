9a78e2db6d8c1dccac8856d053db46dd738ed5a1
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-05-30 21:52:15 -0300

    Eslint changes

--- src/@clean/shared/domain/enums/stack_enum.ts
+++ src/@clean/shared/domain/enums/stack_enum.ts
@@ -1,30 +1,30 @@
 export enum STACK {
-  BACKEND = "BACKEND",
-  FRONTEND = "FRONTEND",
-  INFRA = "INFRA",
-  UX_UI = "UX_UI",
-  PO = "PO",
-  INTERNAL = "INTERNAL",
-  DATA_SCIENCE = "DATA_SCIENCE",
+  BACKEND = 'BACKEND',
+  FRONTEND = 'FRONTEND',
+  INFRA = 'INFRA',
+  UX_UI = 'UX_UI',
+  PO = 'PO',
+  INTERNAL = 'INTERNAL',
+  DATA_SCIENCE = 'DATA_SCIENCE'
 }
 
 export function stackToEnum(value: string): STACK {
   switch (value) {
-    case "BACKEND":
-      return STACK.BACKEND;
-    case "FRONTEND":
-      return STACK.FRONTEND;
-    case "INFRA":
-      return STACK.INFRA;
-    case "UX_UI":
-      return STACK.UX_UI;
-    case "PO":
-      return STACK.PO;
-    case "INTERNAL":
-      return STACK.INTERNAL;
-    case "DATA_SCIENCE":
-      return STACK.DATA_SCIENCE;
+    case 'BACKEND':
+      return STACK.BACKEND
+    case 'FRONTEND':
+      return STACK.FRONTEND
+    case 'INFRA':
+      return STACK.INFRA
+    case 'UX_UI':
+      return STACK.UX_UI
+    case 'PO':
+      return STACK.PO
+    case 'INTERNAL':
+      return STACK.INTERNAL
+    case 'DATA_SCIENCE':
+      return STACK.DATA_SCIENCE
     default:
-      throw new Error("Invalid value");
+      throw new Error('Invalid value')
   }
 }
