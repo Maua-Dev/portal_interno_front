aa4a078b24a80e04fe1c77703329148dba62a29d
Rodrigo <rodrigo.dsiqueira1@gmail.com>
2023-05-31 19:26:35 -0300

    deleted unnecessary files

--- src/@clean/modules/user/domain/repositories/user_repository_interface.ts
+++
@@ -1,8 +0,0 @@
-import { User } from '@/@clean/shared/domain/entities/user'
-
-export interface IUserRepository {
-  createUser(user: User): Promise<User>
-  getUser(userId: number): Promise<User>
-  updateUser(userId: number, newName: string): Promise<User>
-  deleteUser(userId: number): Promise<User>
-}
