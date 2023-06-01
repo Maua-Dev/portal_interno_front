3c4d84e28ca69c74d379475f80914be22fb72552
Vitor Soller <vgsoller@gmail.com>
2023-06-01 10:24:15 -0300

    hosted zone id 👁‍🗨

--- iac/lib/iac-stack.ts
+++ iac/lib/iac-stack.ts
@@ -3,6 +3,8 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as route53Targets from 'aws-cdk-lib/aws-route53-targets';
 
 import { Construct } from 'constructs';
 
@@ -12,6 +14,8 @@ export class IacStack extends cdk.Stack {
     const stage = process.env.GITHUB_REF_NAME || 'dev';
     const acmCertificateArn = process.env.ACM_CERTIFICATE_ARN || 'arn:aws:acm:us-east-1:123456789012:certificate/12345678-1234-1234-1234-123456789012';
     const alternativeDomain = process.env.ALTERNATIVE_DOMAIN || 'onlydevs-dev.devmaua.com';
+    const hostedZoneIdValue = process.env.HOSTED_ZONE_ID || 'Z1UJRXOUMOOFQ8';
+
     const bucket = new s3.Bucket(this, 'PortalInternoFrontBucket' + stage, {
       versioned: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
@@ -41,6 +45,17 @@ export class IacStack extends cdk.Stack {
       minimumProtocolVersion: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
     });
 
+    const zone = route53.HostedZone.fromHostedZoneAttributes(this, 'PortalInternoFrontHostedZone-' + stage, {
+      hostedZoneId: hostedZoneIdValue,
+      zoneName: alternativeDomain,
+    });
+    
+    new route53.ARecord(this, 'PortalInternoFrontAliasRecord-' + stage, {
+      zone: zone,
+      recordName: alternativeDomain,
+      target: route53.RecordTarget.fromAlias(new route53Targets.CloudFrontTarget(cloudfrontDistribution)),
+    });
+    
     new cdk.CfnOutput(this, 'PortalInternoFrontBucketName-' + stage, {
       value: bucket.bucketName,
     });
