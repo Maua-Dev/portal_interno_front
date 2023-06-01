a04495f556cb607e2bf3280e4c3899d16f137034
Vitor Soller <vgsoller@gmail.com>
2023-05-31 23:06:44 -0300

    add certificate and altenative doman

--- iac/lib/iac-stack.ts
+++ iac/lib/iac-stack.ts
@@ -2,13 +2,16 @@ import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
+
 import { Construct } from 'constructs';
 
 export class IacStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     const stage = process.env.GITHUB_REF_NAME || 'dev';
-    
+    const acmCertificateArn = process.env.ACM_CERTIFICATE_ARN || 'arn:aws:acm:us-east-1:123456789012:certificate/12345678-1234-1234-1234-123456789012';
+    const alternativeDomain = process.env.ALTERNATIVE_DOMAIN || 'onlydevs-dev.devmaua.com';
     const bucket = new s3.Bucket(this, 'PortalInternoFrontBucket' + stage, {
       versioned: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
@@ -23,6 +26,8 @@ export class IacStack extends cdk.Stack {
     bucket.grantRead(oai);
 
     const cloudfrontDistribution = new cloudfront.Distribution(this, 'PortalInternoFrontDistribution-' + stage, {
+      domainNames: [alternativeDomain],
+      certificate: Certificate.fromCertificateArn(this, 'PortalInternoFrontCertificate-' + stage, acmCertificateArn),
       comment: 'portal-interno-front-distribution-' + stage,
       defaultBehavior: {
         origin: new origins.S3Origin(bucket,{
