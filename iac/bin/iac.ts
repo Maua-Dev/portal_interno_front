a3fe9f2a65555b630933ad8c19e7a6f804edc4e3
Vitor Soller <vgsoller@gmail.com>
2023-06-02 08:31:06 -0300

    change way to get variables

--- iac/bin/iac.ts
+++ iac/bin/iac.ts
@@ -10,14 +10,8 @@ const env = {
   region: process.env.AWS_REGION
 }
 
-const stage = process.env['GITHUB_REF_NAME'] || 'dev';
-const acmCertificateArn = process.env['ACM_CERTIFICATE_ARN'] || 'arn:aws:acm:us-east-1:123456789012:certificate/12345678-1234-1234-1234-123456789012';
-const alternativeDomain = process.env['ALTERNATIVE_DOMAIN'] || 'onlydevs-dev.devmaua.com';
-const hostedZoneIdValue = process.env['HOSTED_ZONE_ID'] || 'Z1UJRXOUMOOFQ8';
-
-
 const stackName = process.env.STACK_NAME || 'PortalInternoStackDev'
 
-new IacStack(app, stackName, stage, acmCertificateArn, alternativeDomain, hostedZoneIdValue,{
+new IacStack(app, stackName, {
   env: env
 });
