89754192e6039028450145a5811c5adc211d9c47
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-07-05 19:38:38 -0300

    added repo methods

--- src/@clean/shared/infra/repositories/action_repository_mock.ts
+++ src/@clean/shared/infra/repositories/action_repository_mock.ts
@@ -9,6 +9,7 @@ import { STACK } from '../../domain/enums/stack_enum'
 import { COURSE } from '../../domain/enums/course_enum'
 import { ACTIVE } from '../../domain/enums/active_enum'
 import { ACTION_TYPE } from '../../domain/enums/action_type_enum'
+import { NoItemsFoundError } from '../../domain/helpers/errors/domain_error'
 
 export class ActionRepositoryMock implements IActionRepository {
   private projects: Project[] = [
@@ -108,6 +109,20 @@ export class ActionRepositoryMock implements IActionRepository {
       active: ACTIVE.ACTIVE,
       projects: [this.projects[1], this.projects[4]],
       deactivatedDate: 0
+    }),
+    new Member({
+      name: 'MAGIC WHITE HANDS',
+      email: 'jbranco.devmaua@gmail.com',
+      ra: '21002101',
+      role: ROLE.DEV,
+      stack: STACK.BACKEND,
+      year: 3,
+      cellphone: '11911758198',
+      course: COURSE.ECM,
+      hiredDate: 1640192165000,
+      active: ACTIVE.ACTIVE,
+      projects: [this.projects[1], this.projects[4]],
+      deactivatedDate: 0
     })
   ]
   private actions: Action[] = [
@@ -124,20 +139,145 @@ export class ActionRepositoryMock implements IActionRepository {
       stackTags: [STACK.FRONTEND],
       storyId: 1,
       description: 'Reunião de como instalar o yarn'
+    }),
+    new Action({
+      ownerRa: this.members[0].ra,
+      startDate: 1612137600000,
+      endDate: 1612611200000,
+      duration: 3600000,
+      actionId: 'uuid2',
+      associatedMembersRa: [
+        this.members[0].ra,
+        this.members[1].ra,
+        this.members[3].ra
+      ],
+      title: '**Reunião do Front**',
+      actionTypeTags: [ACTION_TYPE.MEETING],
+      projectCode: this.projects[1].code,
+      stackTags: [STACK.FRONTEND],
+      storyId: 1,
+      description: 'Reunião do front'
+    }),
+    new Action({
+      ownerRa: this.members[2].ra,
+      startDate: 1612137600000,
+      endDate: 1612189200000,
+      duration: 3600000,
+      actionId: 'uuid3',
+      associatedMembersRa: [this.members[2].ra, this.members[4].ra],
+      title: '**Reunião do Back**',
+      actionTypeTags: [ACTION_TYPE.MEETING],
+      projectCode: this.projects[1].code,
+      stackTags: [STACK.BACKEND],
+      storyId: 1,
+      description: 'Reunião do back'
+    }),
+    new Action({
+      ownerRa: this.members[2].ra,
+      startDate: 1612137600000,
+      endDate: 1612199200000,
+      duration: 3600000,
+      actionId: 'uuid4',
+      associatedMembersRa: [this.members[2].ra, this.members[4].ra],
+      title: '**Reunião do Back para codar o repo**',
+      actionTypeTags: [ACTION_TYPE.MEETING],
+      projectCode: this.projects[1].code,
+      stackTags: [STACK.BACKEND],
+      storyId: 1,
+      description: 'Reunião do back codando o repo'
     })
   ]
-  private associatedActions: AssociatedAction[] = []
+  private associatedActions: AssociatedAction[] = [
+    new AssociatedAction({
+      member_ra: this.members[0].ra,
+      action: this.actions[0]
+    }),
+    new AssociatedAction({
+      member_ra: this.members[0].ra,
+      action: this.actions[1]
+    }),
+    new AssociatedAction({
+      member_ra: this.members[1].ra,
+      action: this.actions[0]
+    }),
+    new AssociatedAction({
+      member_ra: this.members[1].ra,
+      action: this.actions[1]
+    }),
+    new AssociatedAction({
+      member_ra: this.members[3].ra,
+      action: this.actions[0]
+    }),
+    new AssociatedAction({
+      member_ra: this.members[3].ra,
+      action: this.actions[1]
+    }),
+    new AssociatedAction({
+      member_ra: this.members[2].ra,
+      action: this.actions[2]
+    }),
+    new AssociatedAction({
+      member_ra: this.members[2].ra,
+      action: this.actions[3]
+    }),
+    new AssociatedAction({
+      member_ra: this.members[4].ra,
+      action: this.actions[2]
+    }),
+    new AssociatedAction({
+      member_ra: this.members[4].ra,
+      action: this.actions[3]
+    })
+  ]
 
-  createAction(action: Action): Promise<Action> {
-    throw new Error('Method not implemented.')
+  // createAction(action: Action): Promise<Action> {
+  //   throw new Error('Method not implemented.')
+  // }
+
+  async createAction(action: Action): Promise<Action> {
+    this.actions.push(action)
+    this.createAssociatedAction(
+      new AssociatedAction({
+        member_ra: action.ownerRa,
+        action: action
+      })
+    )
+    if (action.associatedMembersRa.length > 0) {
+      action.associatedMembersRa.forEach((memberRa) => {
+        this.createAssociatedAction(
+          new AssociatedAction({
+            member_ra: memberRa,
+            action: action
+          })
+        )
+      })
+    }
+    return action
   }
-  getAction(actionId: string): Promise<Action> {
-    throw new Error('Method not implemented.')
+
+  // getAction(actionId: string): Promise<Action> {
+  //   throw new Error('Method not implemented.')
+  // }
+
+  async getAction(actionId: string): Promise<Action> {
+    const action = this.actions.find((action) => action.actionId === actionId)
+    if (!action) {
+      throw new NoItemsFoundError(`actionId: ${actionId}`)
+    }
+    return action
   }
-  createAssociatedAction(
+
+  // createAssociatedAction(
+  //   associatedAction: AssociatedAction
+  // ): Promise<AssociatedAction> {
+  //   throw new Error('Method not implemented.')
+  // }
+
+  async createAssociatedAction(
     associatedAction: AssociatedAction
   ): Promise<AssociatedAction> {
-    throw new Error('Method not implemented.')
+    this.associatedActions.push(associatedAction)
+    return associatedAction
   }
 }
 
