96ac1666cc60d08f62654532551ceb6ca728e3f5
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-07-11 21:51:57 -0300

    test invalid duration

--- __tests__/@clean/modules/user/usecases/test_create_action_usecase.test.ts
+++ __tests__/@clean/modules/user/usecases/test_create_action_usecase.test.ts
@@ -111,3 +111,45 @@ test('Test Action with invalid endDate', async () => {
     )
   }).toThrowError('Field props.endDate is not valid')
 })
+
+test('Test Action with invalid duration', async () => {
+  const repo = new ActionRepositoryMock()
+  const actionUsecase = new CreateAction(repo)
+
+  expect(() => {
+    actionUsecase.execute(
+      new Action({
+        ownerRa: '22.40680-0',
+        startDate: 1612137600000,
+        endDate: 1612141200000,
+        duration: -3600000,
+        actionId: 'uuid6',
+        associatedMembersRa: ['22.00680-0', '22.22222-2'],
+        title: '**Reunião**',
+        actionTypeTags: [ACTION_TYPE.MEETING],
+        projectCode: 'PT',
+        stackTags: [STACK.FRONTEND],
+        storyId: 150,
+        description: 'Reunião de como instalar o yarn'
+      })
+    )
+  }).toThrowError(EntityError)
+  expect(() => {
+    actionUsecase.execute(
+      new Action({
+        ownerRa: '22.40680-0',
+        startDate: 1612137600000,
+        endDate: 1612141200000,
+        duration: 36000000,
+        actionId: 'uuid6',
+        associatedMembersRa: ['22.00680-0', '22.22222-2'],
+        title: '**Reunião**',
+        actionTypeTags: [ACTION_TYPE.MEETING],
+        projectCode: 'PT',
+        stackTags: [STACK.FRONTEND],
+        storyId: 150,
+        description: 'Reunião de como instalar o yarn'
+      })
+    )
+  }).toThrowError('Field props.duration is not valid')
+})
