b5d582c6036fe933b1b56f81b967ada154991b53
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-07-11 21:50:23 -0300

    test action usecase, invalid ownerRa, invalid endDate

---
+++ __tests__/@clean/modules/user/usecases/test_create_action_usecase.test.ts
@@ -0,0 +1,113 @@
+import { CreateAction } from '@/@clean/modules/user/usecases/create_action_usecase'
+import { Action } from '@/@clean/shared/domain/entities/action'
+import { ACTION_TYPE } from '@/@clean/shared/domain/enums/action_type_enum'
+import { STACK } from '@/@clean/shared/domain/enums/stack_enum'
+import { EntityError } from '@/@clean/shared/domain/helpers/errors/domain_error'
+import { ActionRepositoryMock } from '@/@clean/shared/infra/repositories/action_repository_mock'
+
+test('Test Action usecase', async () => {
+  const repo = new ActionRepositoryMock()
+  const actionUsecase = new CreateAction(repo)
+
+  const action = new Action({
+    ownerRa: '22.00680-0',
+    startDate: 1612137600000,
+    endDate: 1612141200000,
+    duration: 3600000,
+    actionId: 'uuid1',
+    associatedMembersRa: ['22.00680-0', '22.22222-2'],
+    title: '**Reunião**',
+    actionTypeTags: [ACTION_TYPE.MEETING],
+    projectCode: 'PT',
+    stackTags: [STACK.FRONTEND],
+    storyId: 150,
+    description: 'Reunião de como instalar o yarn'
+  })
+
+  const actionCreated = await actionUsecase.execute(action)
+  expect(actionCreated).toBeInstanceOf(Action)
+})
+
+test('Test Action with invalid ownerRa', async () => {
+  const repo = new ActionRepositoryMock()
+  const actionUsecase = new CreateAction(repo)
+
+  expect(() => {
+    actionUsecase.execute(
+      new Action({
+        ownerRa: '22406800',
+        startDate: 1612137600000,
+        endDate: 1612141200000,
+        duration: 3600000,
+        actionId: 'uuid6',
+        associatedMembersRa: ['22.00680-0', '22.22222-2'],
+        title: '**Reunião**',
+        actionTypeTags: [ACTION_TYPE.MEETING],
+        projectCode: 'PT',
+        stackTags: [STACK.FRONTEND],
+        storyId: 150,
+        description: 'Reunião de como instalar o yarn'
+      })
+    )
+  }).toThrowError(EntityError)
+  expect(() => {
+    actionUsecase.execute(
+      new Action({
+        ownerRa: '22406800',
+        startDate: 1612137600000,
+        endDate: 1612141200000,
+        duration: 3600000,
+        actionId: 'uuid6',
+        associatedMembersRa: ['22.00680-0', '22.22222-2'],
+        title: '**Reunião**',
+        actionTypeTags: [ACTION_TYPE.MEETING],
+        projectCode: 'PT',
+        stackTags: [STACK.FRONTEND],
+        storyId: 150,
+        description: 'Reunião de como instalar o yarn'
+      })
+    )
+  }).toThrowError('Field props.ownerRa is not valid')
+})
+
+test('Test Action with invalid endDate', async () => {
+  const repo = new ActionRepositoryMock()
+  const actionUsecase = new CreateAction(repo)
+
+  expect(() => {
+    actionUsecase.execute(
+      new Action({
+        ownerRa: '22.40680-0',
+        startDate: 1612137600000,
+        endDate: 161214120000,
+        duration: 3600000,
+        actionId: 'uuid6',
+        associatedMembersRa: ['22.00680-0', '22.22222-2'],
+        title: '**Reunião**',
+        actionTypeTags: [ACTION_TYPE.MEETING],
+        projectCode: 'PT',
+        stackTags: [STACK.FRONTEND],
+        storyId: 150,
+        description: 'Reunião de como instalar o yarn'
+      })
+    )
+  }).toThrowError(EntityError)
+  expect(() => {
+    actionUsecase.execute(
+      new Action({
+        ownerRa: '22.40680-0',
+        startDate: 1612137600000,
+        endDate: 161214120000,
+        duration: 3600000,
+        actionId: 'uuid6',
+        associatedMembersRa: ['22.00680-0', '22.22222-2'],
+        title: '**Reunião**',
+        actionTypeTags: [ACTION_TYPE.MEETING],
+        projectCode: 'PT',
+        stackTags: [STACK.FRONTEND],
+        storyId: 150,
+        description: 'Reunião de como instalar o yarn'
+      })
+    )
+  }).toThrowError('Field props.endDate is not valid')
+})
