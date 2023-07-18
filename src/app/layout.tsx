4bfcc13f72582796845dbfcbce738fa9fefefb65
Rodrigo <rodrigo.dsiqueira1@gmail.com>
2023-07-17 21:53:40 -0300

    removed all css modules and default css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body>{children}</body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
 // import { UserProvider } from '@/contexts/user_provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <main className="">
+      <div className="flex flex-col">
         <div>
           <h1 className="text-7xl">P.I: Bem vindo ao nosso Portal Interno</h1>
         </div>
