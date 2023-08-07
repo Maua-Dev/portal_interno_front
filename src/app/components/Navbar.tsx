093ae1a4628208a7a872a41187d81c77de0a1811
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-08-07 15:11:52 -0300

    Componentizing on Navbar file Done

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,7 +6,178 @@ import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 import MenuIcon from '@mui/icons-material/Menu'
 import { DefaultIconButton, NavBarButton } from './little_components/Buttons'
-import { useEffect, useState } from 'react'
+import { ReactNode, useState } from 'react'
+
+// const Container = ({ children }: { children: ReactNode }) => {
+//   return (
+//     <div className="flex h-full w-3/4 flex-col rounded-xl border-2 border-gray-400 px-4">
+//       {children}
+//     </div>
+//   )
+// }
+
+const NavBarContainer = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="flex h-full items-center justify-between bg-gradient-to-t from-blue-900 to-blue-800/90 pl-3 pr-3 text-white lg:pl-11">
+      {children}
+    </div>
+  )
+}
+
+const DevLogo = () => {
+  return (
+    <div className="flex pb-2 pt-2">
+      <h1 className="text-5xl">D</h1>
+      <div className="flex-col">
+        <h2 className="mt-3 leading-none">
+          <strong>EV</strong>
+        </h2>
+        <h3 className="text-xs leading-none">COMMUNITY</h3>
+      </div>
+    </div>
+  )
+}
+
+interface LinkListProps {
+  id: number
+  text: string
+  url: string
+}
+
+const ButtonListWeb = ({ linkList }: { linkList: LinkListProps[] }) => {
+  return (
+    <div className="m-0 flex gap-5  max-[950px]:hidden">
+      {linkList.map((link, index) => {
+        return (
+          <NavBarButton
+            key={index}
+            link_id={link.id}
+            link_text={link.text}
+            button_style=""
+            p_style=""
+          />
+        )
+      })}
+    </div>
+  )
+}
+
+const LeftSide = ({ children }: { children: ReactNode }) => {
+  return <div className="flex gap-12">{children}</div>
+}
+
+interface IconListProps {
+  id: number
+  icon_name: ReactNode
+  url: string
+}
+
+const RightSideIcons = ({
+  handleHamburguerClick,
+  iconList
+}: {
+  handleHamburguerClick: () => void
+  iconList: IconListProps[]
+}) => {
+  return (
+    <div className="flex gap-1 ">
+      {iconList.map((icon, index) => {
+        return (
+          <DefaultIconButton
+            onClick={() => {}}
+            style="text-white max-[950px]:hidden"
+            key={index}
+          >
+            {icon.icon_name}
+          </DefaultIconButton>
+        )
+      })}
+      <DefaultIconButton
+        onClick={handleHamburguerClick}
+        style="text-white min-[950px]:hidden"
+        key={''}
+      >
+        <MenuIcon />
+      </DefaultIconButton>
+    </div>
+  )
+}
+
+const ButtonListMoblie = ({
+  linkList,
+  isOpen
+}: {
+  linkList: LinkListProps[]
+  isOpen: boolean
+}) => {
+  return (
+    <div>
+      {linkList.map((link, index) => {
+        return (
+          <NavBarButton
+            key={index}
+            link_id={link.id}
+            link_text={link.text}
+            button_style={
+              isOpen
+                ? 'w-full flex justify-center py-3 min-[950px]:hidden'
+                : 'hidden'
+            }
+            p_style="text-white"
+          />
+        )
+      })}
+    </div>
+  )
+}
+
+const IconsListMoblie = ({
+  isOpen,
+  iconList
+}: {
+  iconList: IconListProps[]
+  isOpen: boolean
+}) => {
+  return (
+    <div
+      className={
+        isOpen ? 'mt-6 flex items-center justify-center gap-2' : 'hidden'
+      }
+    >
+      {iconList.map((icon, index) => {
+        return (
+          <DefaultIconButton
+            onClick={() => {}}
+            style="text-white min-[950px]:hidden"
+            key={index}
+          >
+            {icon.icon_name}
+          </DefaultIconButton>
+        )
+      })}
+    </div>
+  )
+}
+
+const MobileDropDown = ({
+  isOpen,
+  children
+}: {
+  isOpen: boolean
+  children: ReactNode
+}) => {
+  return (
+    <div
+      className={
+        isOpen
+          ? 'h-fit flex-col justify-evenly gap-3 bg-blue-950 py-3 duration-500 min-[950px]:hidden'
+          : 'relative h-0 duration-700'
+      }
+    >
+      {children}
+    </div>
+  )
+}
 
 export default function NavBar() {
   const [isOpen, setOpen] = useState(false)
@@ -58,102 +229,20 @@ export default function NavBar() {
 
   return (
     <div className="flex-col">
-      <div className="flex h-full items-center justify-between bg-gradient-to-t from-blue-900 to-blue-800/90 pl-3 pr-3 text-white lg:pl-11">
-        <div className="flex gap-12">
-          <div className="flex pb-2 pt-2">
-            <h1 className="text-5xl">D</h1>
-            <div className="flex-col">
-              <h2 className="mt-3 leading-none">
-                <strong>EV</strong>
-              </h2>
-              <h3 className="text-xs leading-none">COMMUNITY</h3>
-            </div>
-          </div>
-          <div className="m-0 flex gap-5  max-[950px]:hidden">
-            {linksList.map((link, index) => {
-              return (
-                <NavBarButton
-                  key={index}
-                  link_id={link.id}
-                  link_text={link.text}
-                  button_style=""
-                  p_style=""
-                />
-              )
-            })}
-          </div>
-        </div>
-        <div className="flex gap-1 ">
-          {iconsList.map((icon, index) => {
-            return (
-              <DefaultIconButton
-                onClick={() => {}}
-                style="text-white max-[950px]:hidden"
-                key={index}
-              >
-                {icon.icon_name}
-              </DefaultIconButton>
-            )
-          })}
-          <DefaultIconButton
-            onClick={handleHamburguerClick}
-            style="text-white min-[950px]:hidden"
-            key={''}
-          >
-            <MenuIcon />
-          </DefaultIconButton>
-        </div>
-      </div>
-      <div
-        className={
-          isOpen
-            ? 'h-fit flex-col justify-evenly gap-3 bg-blue-950 py-3 duration-500 min-[950px]:hidden'
-            : 'relative h-0 duration-700'
-        }
-      >
-        <div>
-          {linksList.map((link, index) => {
-            return (
-              <NavBarButton
-                key={index}
-                link_id={link.id}
-                link_text={link.text}
-                button_style={
-                  isOpen
-                    ? 'w-full flex justify-center py-3 min-[950px]:hidden'
-                    : 'hidden'
-                }
-                p_style="text-white"
-              />
-            )
-          })}
-        </div>
-        <div>
-          <div
-            className={
-              isOpen ? 'mt-6 flex items-center justify-center gap-2' : 'hidden'
-            }
-          >
-            {iconsList.map((icon, index) => {
-              return (
-                <DefaultIconButton
-                  onClick={() => {}}
-                  style="text-white min-[950px]:hidden"
-                  key={index}
-                >
-                  {icon.icon_name}
-                </DefaultIconButton>
-              )
-            })}
-            <DefaultIconButton
-              onClick={handleHamburguerClick}
-              style="text-white max-[950px]:hidden"
-            >
-              <MenuIcon />
-            </DefaultIconButton>
-          </div>
-        </div>
-      </div>
+      <NavBarContainer>
+        <LeftSide>
+          <DevLogo />
+          <ButtonListWeb linkList={linksList} />
+        </LeftSide>
+        <RightSideIcons
+          handleHamburguerClick={handleHamburguerClick}
+          iconList={iconsList}
+        />
+      </NavBarContainer>
+      <MobileDropDown isOpen={isOpen}>
+        <ButtonListMoblie isOpen={isOpen} linkList={linksList} />
+        <IconsListMoblie isOpen={isOpen} iconList={iconsList} />
+      </MobileDropDown>
     </div>
   )
 }
