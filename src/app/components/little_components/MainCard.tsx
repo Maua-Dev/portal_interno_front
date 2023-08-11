af0a80ad49536f6022227cd97b8d2740289b382d
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2023-08-11 14:45:53 -0300

    added height attribute to MainCard component

--- src/app/components/little_components/MainCard.tsx
+++ src/app/components/little_components/MainCard.tsx
@@ -3,14 +3,19 @@ import { ReactNode } from 'react'
 interface MainCardProps {
   children: ReactNode
   width?: string | undefined
+  height?: string | undefined
 }
 
-const MainCard = ({ children, width }: MainCardProps) => {
+const MainCard = ({ children, width, height }: MainCardProps) => {
   const widthApplied = width === undefined ? 'w-1/2' : width
+  const heightApplied = height === undefined ? 'h-fit' : height
   return (
     <div
       className={
-        'h-fit rounded-xl border-2 border-gray-400 px-7 py-2 ' + widthApplied
+        'rounded-xl border-2 border-gray-400 px-7 py-2 ' +
+        widthApplied +
+        ' ' +
+        heightApplied
       }
     >
       {children}
