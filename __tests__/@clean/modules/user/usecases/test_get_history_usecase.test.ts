97fb965af317d89305fd2477c096b8c85294e72f
Enzo Sakamoto <enzo.sak@hotmail.com>
2023-08-26 18:23:40 -0300

    fixed get history tests

--- __tests__/@clean/modules/user/usecases/test_get_history_usecase.test.ts
+++ __tests__/@clean/modules/user/usecases/test_get_history_usecase.test.ts
@@ -8,14 +8,14 @@ test('Test Get History Usecase', async () => {
 
   const { actions, lastId } = await getHistoryUsecase.execute('21.00210-0', 10)
 
-  expect(actions.length).toBe(6)
+  expect(actions.length).toBe(4)
   expect(actions[0].title).toBe('Componente Histórico e Atividades')
   expect(actions[0].description).toBe('Reunião Daily')
 
   expect(actions[1].title).toBe('Daily')
   expect(actions[1].description).toBe('Reunião Daily')
 
-  expect(lastId).toBe('uuid1')
+  expect(lastId).toBe('uuid5')
 })
 
 test('Test Get History Usecase with start and end', async () => {
@@ -25,15 +25,15 @@ test('Test Get History Usecase with start and end', async () => {
   const { actions, lastId } = await getHistoryUsecase.execute(
     '21.00210-0',
     10,
-    1612137600000,
-    1612141200000
+    1689966000000,
+    1689976380000
   )
 
-  expect(actions.length).toBe(1)
-  expect(actions[0].title).toBe('**Reunião**')
-  expect(actions[0].description).toBe('Reunião de como instalar o yarn')
+  expect(actions.length).toBe(2)
+  expect(actions[0].title).toBe('Componente Histórico e Atividades')
+  expect(actions[0].description).toBe('Reunião Daily')
 
-  expect(lastId).toBe('uuid1')
+  expect(lastId).toBe('uuid7')
 })
 
 test('Test Get History Usecase with start and end - example 2', async () => {
@@ -43,15 +43,15 @@ test('Test Get History Usecase with start and end - example 2', async () => {
   const { actions, lastId } = await getHistoryUsecase.execute(
     '21.00210-0',
     10,
-    1612141200000,
-    1612144800000
+    1689955200000,
+    1689976380000
   )
 
-  expect(actions.length).toBe(1)
-  expect(actions[0].title).toBe('**Reunião do Front**')
-  expect(actions[0].description).toBe('Reunião do front')
+  expect(actions.length).toBe(3)
+  expect(actions[1].title).toBe('Daily')
+  expect(actions[1].description).toBe('Reunião Daily')
 
-  expect(lastId).toBe('uuid2')
+  expect(lastId).toBe('uuid6')
 })
 
 test('Test Get History Usecase with exclusiveStartKey', async () => {
@@ -63,12 +63,12 @@ test('Test Get History Usecase with exclusiveStartKey', async () => {
     10,
     undefined,
     undefined,
-    'uuid6'
+    'uuid7'
   )
 
-  expect(actions.length).toBe(3)
-  expect(actions[0].title).toBe('Reunião do Front')
-  expect(actions[0].description).toBe('Reunião do front')
+  expect(actions.length).toBe(2)
+  expect(actions[1].title).toBe('Reunião do Front')
+  expect(actions[1].description).toBe('Reunião do front')
 
-  expect(lastId).toBe('uuid1')
+  expect(lastId).toBe('uuid5')
 })
