f3185de5627249f810d7d7bae06e6fdcaa3d079d
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-02-19 10:13:45 -0300

    added new functions

--- src/app/utils/functions/timeStamp.ts
+++ src/app/utils/functions/timeStamp.ts
@@ -1,4 +1,4 @@
-function timeStampToDate(timestamp: number): string {
+export function timeStampToDate(timestamp: number): string {
   const date = new Date(timestamp * 1000)
   const day = date.getDate().toString().padStart(2, '0')
   const month = (date.getMonth() + 1).toString().padStart(2, '0') // O mês é base 0, então adiciona 1
@@ -7,7 +7,18 @@ function timeStampToDate(timestamp: number): string {
   return `${day}/${month}/${year}`
 }
 
-function secondsToDays(seconds: number): number {
-  const secondsPerDay = 60 * 60 * 24
-  return Math.floor(seconds / secondsPerDay)
+export function millisecondsToHours(milliseconds: number): number {
+  return Math.floor(milliseconds / Math.pow(3.6, 6))
+}
+
+export function hoursToMilliseconds(hours: number): number {
+  return Math.floor(hours * Math.pow(3.6, 6))
+}
+
+export function dateToMilliseconds(data: string): number {
+  const [day, month, year] = data.split('/')
+
+  const newData = new Date(`${day}-${month}-${year}`)
+
+  return newData.getTime()
 }
