8f5ffa434af31efd0ec70e96b73ad7389b63ca40
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-03-04 21:44:54 -0300

    add action for tests

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -3,10 +3,27 @@ import ActionModal from '../components/ActionModal'
 import Navbar from '../components/Navbar'
 import { ModalContext } from '../contexts/modal_context'
 import useDarkMode from '../utils/functions/useDarkMode'
+import { Action } from '../../@clean/shared/domain/entities/action'
+import { ACTION_TYPE } from '../../@clean/shared/domain/enums/action_type_enum'
+import { STACK } from '../../@clean/shared/domain/enums/stack_enum'
 
 export default function Home() {
   const { darkMode } = useDarkMode()
   const { isModalOpen } = useContext(ModalContext)
+  const action = new Action({
+    ownerRa: '21002100',
+    startDate: 1689969600000,
+    endDate: 1689976380000,
+    duration: 6780000,
+    actionId: 'uuid8',
+    title: 'Componente Histórico e Atividades',
+    actionTypeTag: ACTION_TYPE.MEETING,
+    projectCode: 'MF',
+    stackTags: [STACK.FRONTEND],
+    storyId: 150,
+    description: 'Reunião Daily',
+    associatedMembersRa: ['21002102', '21002101']
+  })
 
   return (
     <>
@@ -14,7 +31,7 @@ export default function Home() {
       <main
         className={`h-screen w-full ${darkMode ? 'bg-black' : 'bg-sky-200'}`}
       >
-        {isModalOpen && <ActionModal />}
+        {isModalOpen && <ActionModal action={action} />}
       </main>
     </>
   )
