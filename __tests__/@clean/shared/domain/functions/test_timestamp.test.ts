2cf4cef0f720feee80dd07b4946dfcb91ba9fa55
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-03-18 19:45:17 -0300

    add new function test

--- __tests__/@clean/shared/domain/functions/test_timestamp.test.ts
+++ __tests__/@clean/shared/domain/functions/test_timestamp.test.ts
@@ -1,10 +1,21 @@
-import { dateToMilliseconds } from '../../../../../src/app/utils/functions/timeStamp'
+import {
+  dateToMilliseconds,
+  hoursToMilliseconds
+} from '../../../../../src/app/utils/functions/timeStamp'
 import '@testing-library/jest-dom'
 
 test('Date to timestamp', () => {
-  const date = '20/07/2002'
+  const date = '2024-03-18T18:33:54'
 
   const timestamp = dateToMilliseconds(date)
 
-  expect(timestamp).toBe(1027123200000)
+  expect(timestamp).toBe(1710797634000)
+})
+
+test('Milliseconds to hour', () => {
+  const hours = 2
+
+  const milliseconds = hoursToMilliseconds(hours)
+
+  expect(milliseconds).toBe(7200000)
 })
