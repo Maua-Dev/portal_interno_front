a849c243e4cec6dadb65be75e4509c0db3ecfd82
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-04-18 19:19:05 -0300

    add home validation

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -2,39 +2,24 @@ import { useContext, useEffect } from 'react'
 import Navbar from '../components/Navbar'
 import { ModalContext } from '../contexts/modal_context'
 import useDarkMode from '../utils/functions/useDarkMode'
-import { ROLE } from '../../@clean/shared/domain/enums/role_enum'
-import { STACK } from '../../@clean/shared/domain/enums/stack_enum'
-import { COURSE } from '../../@clean/shared/domain/enums/course_enum'
+import { useNavigate } from 'react-router-dom'
 import { MemberContext } from '../contexts/member_context'
-// import { Action } from '../../@clean/shared/domain/entities/action'
-// import { ACTION_TYPE } from '../../@clean/shared/domain/enums/action_type_enum'
-// import { STACK } from '../../@clean/shared/domain/enums/stack_enum'
 
 export default function Home() {
   const { darkMode } = useDarkMode()
   const { modalContent } = useContext(ModalContext)
-  const { createMember, memberError } = useContext(MemberContext)
-
-  async function batchCreateMember() {
-    const member = await createMember(
-      '22006800',
-      'rsiqueira.devmaua@gmail.com',
-      ROLE.DEV,
-      STACK.FRONTEND,
-      3,
-      '11942318600',
-      COURSE.CIC
-    )
-
-    console.log(member)
-  }
+  const { getMember } = useContext(MemberContext)
+  const navigate = useNavigate()
 
   useEffect(() => {
-    batchCreateMember()
-    if (memberError) {
-      return
-    }
-  }, [batchCreateMember])
+    getMember().then((res) => {
+      const member = res?.props
+      if (!member) {
+        localStorage.removeItem('idToken')
+        navigate('/login')
+      }
+    })
+  }, [getMember, navigate])
 
   return (
     <>
@@ -44,9 +29,7 @@ export default function Home() {
           darkMode ? 'bg-skin-fill' : 'theme-white bg-sky-200'
         }`}
       >
-        {/* {isModalOpen && <ActionModal action={action} />} */}
         {modalContent}
-        {/* {isModalOpen && <ActionModal />} */}
       </main>
     </>
   )
