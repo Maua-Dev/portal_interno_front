586a477328121def705df2ebb52053aeacae6e9d
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-04-23 18:15:08 -0300

    add darkmode

--- src/app/components/Selector/components/Row.tsx
+++ src/app/components/Selector/components/Row.tsx
@@ -1,8 +1,14 @@
 import { IoMdClose } from 'react-icons/io'
+import useDarkMode from '../../../utils/functions/useDarkMode'
 
 export function Row({ text, onClick }: { text: string; onClick: () => void }) {
+  const { darkMode } = useDarkMode()
   return (
-    <div className="flex items-center justify-between rounded-lg bg-gray-300 p-2 text-base">
+    <div
+      className={`flex items-center justify-between rounded-lg p-2 text-base ${
+        darkMode ? 'bg-gray-600' : 'bg-gray-300'
+      }`}
+    >
       <p className="w-3/4">{text}</p>
       <IoMdClose onClick={onClick} className="cursor-pointer" />
     </div>
