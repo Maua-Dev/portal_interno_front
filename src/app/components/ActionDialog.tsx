d61cd301a5853c7c40200bd6b4c23ffa8896bf56
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-05-03 01:27:48 -0300

    fix: mobile height fix

--- src/app/components/ActionDialog.tsx
+++ src/app/components/ActionDialog.tsx
@@ -61,7 +61,7 @@ export default function ActionDialog({ action, children }: ActionDialogProps) {
         <DialogPrimitive.Trigger asChild>{children}</DialogPrimitive.Trigger>
         <DialogPrimitive.Overlay className="fixed inset-0 z-40 opacity-70 backdrop-blur-sm" />
         <DialogPrimitive.DialogContent
-          className={`scrollbar-hide-default absolute top-[11%] z-50 my-auto flex h-5/6 w-5/6 flex-col gap-16 overflow-x-hidden overflow-y-scroll rounded-md border border-skin-muted px-4 py-10 text-skin-base outline-none sm:w-4/6 md:px-10 lg:h-4/6 xl:w-6/12 xl:scrollbar-hide ${
+          className={`scrollbar-hide-default absolute top-[9%] z-50 my-auto flex h-[80%] w-5/6 flex-col gap-16 overflow-x-hidden overflow-y-scroll rounded-md border border-skin-muted px-4 py-10 text-skin-base outline-none sm:w-4/6 md:px-10 lg:h-4/6 xl:w-6/12 xl:scrollbar-hide ${
             theme ? 'bg-skin-fill' : 'bg-skin-secundary'
           }`}
         >
