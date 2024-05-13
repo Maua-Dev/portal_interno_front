bec3d6fa031c70f3a580a42bf8a969b602d9645f
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-05-13 16:31:35 -0300

    fix lint

--- src/app/contexts/project_context.tsx
+++ src/app/contexts/project_context.tsx
@@ -1,38 +1,43 @@
-import { createContext } from "react"
-import { Project } from "../../@clean/shared/domain/entities/project"
-import { RegistryProject, containerProject } from "../../@clean/shared/infra/containers/container_project"
-import { GetAllProjectsUsecase } from "../../@clean/modules/project/usecases/get_all_projects_usecase"
+import { createContext } from 'react'
+import {
+  RegistryProject,
+  containerProject
+} from '../../@clean/shared/infra/containers/container_project'
+import { GetAllProjectsUsecase } from '../../@clean/modules/project/usecases/get_all_projects_usecase'
+import { ProjectType } from '../../@clean/shared/infra/repositories/project_repository_http'
 
 export type ProjectContextType = {
-    getAllProjects: () => Promise<Project[]>
+  getAllProjects: () => Promise<ProjectType[]>
 }
 
 const projectContextDefault: ProjectContextType = {
-    getAllProjects: async () => {
-        return []
-    }
+  getAllProjects: async () => {
+    return []
+  }
 }
 
-export const ProjectContext = createContext<ProjectContextType>(projectContextDefault)
+export const ProjectContext = createContext<ProjectContextType>(
+  projectContextDefault
+)
 
 const getAllProjectsUsecase = containerProject.get<GetAllProjectsUsecase>(
-    RegistryProject.getAllProjectsUsecase
+  RegistryProject.getAllProjectsUsecase
 )
 
 export function ProjectProvider({ children }: { children: React.ReactNode }) {
-    const getAllProjects = async () => {
-        try {
-            const projects = await getAllProjectsUsecase.execute()
+  const getAllProjects = async () => {
+    try {
+      const projects = await getAllProjectsUsecase.execute()
 
-            return projects
-        } catch (error: any) {
-            throw new Error(error)
-        }
+      return projects
+    } catch (error: any) {
+      throw new Error(error)
     }
+  }
 
-    return (
-        <ProjectContext.Provider value={{ getAllProjects }}>
-            {children}
-        </ProjectContext.Provider>
-    )
+  return (
+    <ProjectContext.Provider value={{ getAllProjects }}>
+      {children}
+    </ProjectContext.Provider>
+  )
 }
