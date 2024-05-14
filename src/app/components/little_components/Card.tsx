29423b848c3b21b34c32f2202d5bef94600d8172
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-05-14 15:42:50 -0300

    fix: card ref error

--- src/app/components/little_components/Card.tsx
+++ src/app/components/little_components/Card.tsx
@@ -1,23 +1,28 @@
-import { HTMLAttributes } from 'react'
+import { HTMLAttributes, forwardRef } from 'react'
 import { twMerge } from 'tailwind-merge'
 
-type variantType = 'sm' | 'md' | 'lg'
+type VariantType = 'sm' | 'md' | 'lg'
 
 interface CardProps extends HTMLAttributes<HTMLDivElement> {
-  variant: variantType
+  variant: VariantType
 }
 
-export default function Card({ variant, children, ...props }: CardProps) {
-  const variants: Record<variantType, string> = {
-    sm: '',
-    md: '',
-    lg: 'h-3/5 w-4/5 bg-skin-secundary p-4 text-skin-base'
+// eslint-disable-next-line react/display-name
+const Card = forwardRef<HTMLDivElement, CardProps>(
+  ({ variant, children, ...props }, ref) => {
+    const variants: Record<VariantType, string> = {
+      sm: '',
+      md: '',
+      lg: 'h-3/5 w-4/5 bg-skin-secundary p-4 text-skin-base'
+    }
+
+    const style = variants[variant]
+    return (
+      <div {...props} ref={ref} className={twMerge(style, props.className)}>
+        {children}
+      </div>
+    )
   }
+)
 
-  const style = variants[variant]
-  return (
-    <div {...props} className={twMerge(style, props.className)}>
-      {children}
-    </div>
-  )
-}
+export default Card
