bf10cdb2bd00dff0c6030b973b6da868dce24dd9
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-05-24 09:37:02 -0300

    fix: remove action entity validations

--- src/@clean/modules/action/usecases/update_action_usecase.ts
+++ src/@clean/modules/action/usecases/update_action_usecase.ts
@@ -1,7 +1,6 @@
 import { Action } from '../../../shared/domain/entities/action'
 import { ACTION_TYPE } from '../../../shared/domain/enums/action_type_enum'
 import { STACK } from '../../../shared/domain/enums/stack_enum'
-import { EntityError } from '../../../shared/domain/helpers/errors/domain_error'
 import { IActionRepository } from '../domain/repositories/action_repository_interface'
 
 export class UpdateActionUsecase {
@@ -21,62 +20,6 @@ export class UpdateActionUsecase {
     newStackTags?: STACK[],
     newActionTypeTag?: ACTION_TYPE
   ): Promise<Action> {
-    if (!Action.validateActionId(actionId)) {
-      throw new EntityError('actionId')
-    }
-
-    if (newStartDate && !Action.validateStartDate(newStartDate)) {
-      throw new EntityError('startDate')
-    }
-
-    if (newEndDate && newStartDate) {
-      if (!Action.validateEndDate(newEndDate)) throw new EntityError('endDate')
-    }
-
-    if (newIsValid && typeof newIsValid !== 'boolean') {
-      throw new EntityError('isValid')
-    }
-
-    if (
-      newEndDate &&
-      newStartDate &&
-      newDuration &&
-      !Action.validateDuration(newDuration, newStartDate, newEndDate)
-    ) {
-      throw new EntityError('duration')
-    }
-
-    if (newStoryId && !Action.validateStoryId(newStoryId)) {
-      throw new EntityError('storyId')
-    }
-
-    if (newTitle && !Action.validateTitle(newTitle)) {
-      throw new EntityError('title')
-    }
-
-    if (newDescription && !Action.validateDescription(newDescription)) {
-      throw new EntityError('description')
-    }
-
-    if (newProjectCode && !Action.validateProjectCode(newProjectCode)) {
-      throw new EntityError('projectCode')
-    }
-
-    if (
-      newAssociatedMembersUserÌds &&
-      !Action.validateAssociatedMembersUserIds(newAssociatedMembersUserÌds)
-    ) {
-      throw new EntityError('associatedMembersUserIds')
-    }
-
-    if (newStackTags && !Action.validateStackTags(newStackTags)) {
-      throw new EntityError('stackTags')
-    }
-
-    if (newActionTypeTag && !Action.validateActionTypeTag(newActionTypeTag)) {
-      throw new EntityError('actionTypeTag')
-    }
-
     return this.actionRepo.updateAction(
       actionId,
       newStartDate,
--- src/@clean/modules/member/usecases/get_member_usecase.ts
+++ src/@clean/modules/member/usecases/get_member_usecase.ts
@@ -1,17 +1,10 @@
 import { Member } from '../../../shared/domain/entities/member'
-import { NoItemsFoundError } from '../../../shared/domain/helpers/errors/domain_error'
 import { IMemberRepository } from '../../member/domain/repositories/member_repository_interface'
 
 export class GetMemberUsecase {
   constructor(private actionRepo: IMemberRepository) {}
 
   async execute(): Promise<Member> {
-    const member = await this.actionRepo.getMember()
-
-    if (!member) {
-      throw new NoItemsFoundError('No member found')
-    }
-
-    return member
+    return await this.actionRepo.getMember()
   }
 }
