05ec6ced7ba301cd9107cb9985126b369bae96af
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-07-14 22:30:56 -0300

    feat: projects page with api data

--- src/app/components/Projects/components/ProjectCard.tsx
+++ src/app/components/Projects/components/ProjectCard.tsx
@@ -4,20 +4,19 @@ import Card from '../../Card'
 import { IconText, StateIcon } from '../../Historic/components/Icon'
 import HoverCard from '../../HoverCard'
 import { Calendar } from 'lucide-react'
+import { ProjectType } from '../../../../@clean/shared/infra/repositories/project_repository_http'
+import { useContext } from 'react'
+import { MemberContext } from '../../../contexts/member_context'
 
 interface ProjectCardProps {
-  name: string
-  description: string
-  poUserId: string
-  scrumUserId: string
-  startDate: number
+  project: ProjectType
 }
 
-const MOCK_DESCRIPTION =
-  'O Gerenciador de Tarefas Colaborativo é uma aplicação web inovadora desenvolvida para ajudar equipes a gerenciar suas tarefas e projetos de forma eficiente e colaborativa. A plataforma permite que os usuários criem, organizem e acompanhem tarefas, promovendo uma comunicação fluida e uma gestão de tempo otimizada. Com uma interface intuitiva e recursos poderosos, o Gerenciador de Tarefas Colaborativo é a solução ideal para equipes de todos os tamanhos que buscam aumentar a produtividade e melhorar a colaboração.'
-
-export default function ProjectCard() {
+export default function ProjectCard({ project }: ProjectCardProps) {
   const { darkMode } = useDarkMode()
+  const { allMembers } = useContext(MemberContext)
+
+  const startDateFormated = new Date(project.startDate).toLocaleDateString()
 
   return (
     <Card
@@ -26,35 +25,51 @@ export default function ProjectCard() {
         'static flex h-fit cursor-pointer flex-col items-center justify-between gap-5 pr-6 shadow-sm shadow-gray-500 brightness-95 duration-150 ease-in hover:brightness-100 sm:flex-row sm:gap-0'
       }
     >
-      <div className="flex flex-row items-center justify-between gap-2">
-        <StateIcon variant={'approved'} />
-        <div className="flex flex-col gap-1">
-          <p className="pl-2 text-lg font-semibold">Portal Interno</p>
-          <div className="flex flex-row gap-2 rounded-full bg-skin-fill pr-2 text-sm text-white ">
-            <HoverCard side="top" placeholder={'PO'}>
-              <p className="flex items-center justify-center rounded-full bg-skin-button-blue-accent px-2 text-center">
-                Fernando Azevedo
-              </p>
-            </HoverCard>
-            <HoverCard side="top" placeholder="SCRUM">
-              <p
-                className={`flex items-center justify-center text-center ${
-                  darkMode ? 'text-white' : 'text-black'
-                }`}
-              >
-                Bernardo
-              </p>
-            </HoverCard>
+      <div className="flex w-full flex-row items-center gap-4">
+        <div className="flex flex-row items-center justify-start gap-2 md:w-3/6">
+          <StateIcon variant={'approved'} />
+          <div className="flex flex-col gap-1">
+            <p className="pl-2 text-lg font-semibold">{project.name}</p>
+            <div className="flex flex-row gap-2 rounded-full bg-skin-fill pr-2 text-sm text-white ">
+              <HoverCard side="top" placeholder={'PO'}>
+                <p className="flex items-center justify-center rounded-full bg-skin-button-blue-accent px-2 text-center">
+                  {allMembers?.find(
+                    (member) => member.userId === project.poUserId
+                  )
+                    ? allMembers.find(
+                        (member) => member.userId === project.poUserId
+                      )?.name
+                    : 'Not Found'}
+                </p>
+              </HoverCard>
+              <HoverCard side="top" placeholder="SCRUM">
+                <p
+                  className={`flex items-center justify-center text-center ${
+                    darkMode ? 'text-white' : 'text-black'
+                  }`}
+                >
+                  {allMembers?.find(
+                    (member) => member.userId === project.scrumUserId
+                  )
+                    ? allMembers.find(
+                        (member) => member.userId === project.scrumUserId
+                      )?.name
+                    : 'Not Found'}
+                </p>
+              </HoverCard>
+            </div>
           </div>
         </div>
+        <p>
+          {project.description.length > 60
+            ? project.description.slice(0, 60) + ' ...'
+            : project.description}
+        </p>
       </div>
-      <p>
-        {MOCK_DESCRIPTION.length > 60
-          ? MOCK_DESCRIPTION.slice(0, 60) + ' ...'
-          : MOCK_DESCRIPTION}
-      </p>
-      <IconText text={'10/07/2002'} icon={Calendar} />
-      <BsThreeDots className="h-10 w-10 cursor-pointer p-2 text-skin-base" />
+      <div className="flex flex-row items-center gap-3">
+        <IconText text={startDateFormated} icon={Calendar} />
+        <BsThreeDots className="h-10 w-10 cursor-pointer p-2 text-skin-base" />
+      </div>
     </Card>
   )
 }
--- src/app/components/Projects/index.tsx
+++ src/app/components/Projects/index.tsx
@@ -1,15 +1,28 @@
-import { useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import FilterBar, { FilterProps } from '../FilterBar'
 import { projectFilterOptions } from './filterOptions'
 import { Monitor } from 'lucide-react'
 import ProjectCard from './components/ProjectCard'
+import { ProjectContext } from '../../contexts/project_context'
+import { ProjectType } from '../../../@clean/shared/infra/repositories/project_repository_http'
 
 export default function Projects() {
+  const { getAllProjects } = useContext(ProjectContext)
+  const [projects, setProjects] = useState<ProjectType[] | undefined>(undefined)
   const [filterProps, setFilterProps] = useState<FilterProps>({
     searchText: '',
     active: '',
     orderBy: ''
   })
+
+  async function loadAllProjects() {
+    const projects = await getAllProjects()
+    setProjects(projects)
+  }
+
+  useEffect(() => {
+    loadAllProjects()
+  }, [])
 
   return (
     <div className="flex h-fit w-full flex-col items-center justify-center gap-2 py-20 pl-0 md:py-10 md:pl-14">
@@ -26,9 +39,10 @@ export default function Projects() {
           3 < 10 ? 'h-screen' : null
         } `}
       >
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        {projects &&
+          projects.map((project, index) => {
+            return <ProjectCard key={project.name + index} project={project} />
+          })}
       </div>
     </div>
   )
