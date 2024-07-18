8716b323feaae146d1725f730266f39f42d91e63
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-07-17 22:49:19 -0300

    fix: fix text color

--- src/app/components/OnHoldModal.tsx
+++ src/app/components/OnHoldModal.tsx
@@ -6,10 +6,10 @@ export function OnHoldModal() {
     <div className="flex h-screen w-full items-center justify-center">
       <div className="absolute z-50 h-full w-full bg-black/80"></div>
       <div
-        className={`z-[60] flex w-2/5 flex-col items-center justify-between gap-12 rounded-lg sm:w-1/2 sm:gap-6 sm:p-6 ${
+        className={`z-[60] flex w-2/3 max-w-xl flex-col items-center justify-between gap-12 rounded-lg p-6 sm:w-1/2 sm:gap-6 ${
           darkMode
             ? 'sm:bg-dev-gray sm:shadow-[1px_1px_0px_9px_rgba(255,255,255,0.34)]'
-            : 'sm:bg-white sm:shadow-[1px_1px_0px_9px_rgba(176,173,173,0.74)]'
+            : 'bg-white shadow-[1px_1px_0px_9px_rgba(176,173,173,0.74)]'
         }`}
       >
         <p className={`w-full ${darkMode ? 'text-white' : 'text-black'}`}>
