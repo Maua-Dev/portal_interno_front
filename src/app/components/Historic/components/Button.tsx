2c86e17d82d9d05c3341e7093fe7e840a84e3ca0
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-09-04 19:16:51 -0300

    fix: button style

--- src/app/components/Historic/components/Button.tsx
+++ src/app/components/Historic/components/Button.tsx
@@ -22,7 +22,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
     const { darkMode } = useDarkMode()
     const variantsStyle: Record<VariantTypes, string> = {
       default:
-        'flex flex-row items-center gap-2 outline-none rounded-sm bg-skin-secundary px-4 py-2 font-semibold duration-150 ease-in hover:bg-skin-fill',
+        'flex flex-row items-center justify-center gap-2 outline-none rounded-sm bg-skin-secundary px-4 py-2 font-semibold duration-150 ease-in hover:bg-skin-fill',
       form: 'flex flex-row items-center justify-center gap-2 outline-none text-white rounded-sm bg-skin-button-blue-accent px-4 py-2 font-semibold duration-150 ease-in hover:bg-skin-button-blue-accent-hover',
       icon: 'p-1 rounded-full duration-150 hover:bg-skin-fill',
       destructive: `flex flex-row items-center gap-2 outline-none rounded-sm text-white px-4 py-2 font-semibold duration-150 ease-in ${
