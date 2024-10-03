be43072df5ce665bac148510135a48c45860c11d
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-10-02 23:11:14 -0300

    feat: badge ui

--- src/app/components/Badge.tsx
+++ src/app/components/Badge.tsx
@@ -6,7 +6,7 @@ export default function Badge({ ...props }: HTMLAttributes<HTMLDivElement>) {
   const { darkMode } = useContext(ThemeContext)
 
   const style =
-    'rounded-md bg-skin-fill px-3 py-1 text-base ' +
+    'rounded-md bg-skin-fill px-3 py-1 md:text-sm lg:text-base ' +
     (darkMode ? 'hover:bg-zinc-800' : 'hover:bg-zinc-400/60')
   return <p className={twMerge(style, props.className)}>{props.children}</p>
 }
