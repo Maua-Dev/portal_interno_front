144ceb50c180c4567a98f70d99678efae45353d9
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-10-03 23:02:17 -0300

    feat: avatar ui

--- src/app/components/Avatar.tsx
+++ src/app/components/Avatar.tsx
@@ -1,17 +1,25 @@
+import { twMerge } from 'tailwind-merge'
+
 type AvatarProps = React.HTMLAttributes<HTMLDivElement> & {
   name: string
 }
 
 export function Avatar({ name, ...props }: AvatarProps) {
+  const splitName = name.trim().split(' ')
+  const firstName = splitName[0]
+  const lastName = splitName.length > 1 ? splitName[splitName.length - 1] : ''
+
   return (
     <div
-      // className={` ${className}`}
+      className={twMerge(
+        'flex aspect-[1/1] h-8 w-8 items-center justify-center rounded-full bg-skin-secundary text-skin-muted hover:text-skin-base',
+        props.className
+      )}
       {...props}
     >
-      {name
-        .split(' ')
-        .map((n) => n[0])
-        .join('')}
+      <p className="text-sm font-medium">
+        {lastName ? `${firstName[0]}${lastName[0]}` : `${firstName[0]}`}
+      </p>
     </div>
   )
 }
