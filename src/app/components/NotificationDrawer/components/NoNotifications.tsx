61e0e6ad3abb59e655555508a99a5f09e73d6d49
Bruno Augusto Lopes Fevereiro <75047754+BRUNO-FEVE@users.noreply.github.com>
2024-10-05 21:15:56 -0300

    feat: ui done

--- src/app/components/NotificationDrawer/components/NoNotifications.tsx
+++ src/app/components/NotificationDrawer/components/NoNotifications.tsx
@@ -3,7 +3,7 @@ import { BellOff } from 'lucide-react'
 export default function NoNotifications() {
   return (
     <div className="mt-36 flex h-full w-full justify-center pt-20">
-      <div className="flex flex-col items-center gap-3 text-gray-500">
+      <div className="flex flex-col items-center gap-3 text-skin-muted">
         <BellOff size={48} className="text-gray-400" />
         <p className="text-lg font-semibold">Você está atualizado!</p>
         <p className="text-sm">Sem novas notificações por enquanto.</p>
