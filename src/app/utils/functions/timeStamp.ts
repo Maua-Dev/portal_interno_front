715a2dfdff40f62b163c60dd93473120800758ed
tokujiTO <tiagomassuda123@gmail.com>
2025-03-08 12:04:40 -0300

    feat: fix millisecondstohour function

--- src/app/utils/functions/timeStamp.ts
+++ src/app/utils/functions/timeStamp.ts
@@ -33,7 +33,7 @@ export function timeStampToDateDDMMYY(timestamp: number): string {
 }
 
 export function millisecondsToHours(milliseconds: number): number {
-  return Math.round(milliseconds / (3.6 * Math.pow(10, 6))*100)/100
+  return Number((milliseconds / (3.6 * Math.pow(10, 6))).toFixed(2))
 }
 
 export function hoursToMilliseconds(hours: number): number {
