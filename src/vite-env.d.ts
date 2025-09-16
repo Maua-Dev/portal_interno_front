5df40835fab3188838c5af0dd2ba31608a944ef3
Vitor Soller <vgsoller@gmail.com>
2025-09-16 02:16:26 -0300

    feat: add enviroments

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_STAGE: string
   readonly VITE_MSS_API_URL: string
   readonly VITE_REFRESH_TOKEN_URL: string
+  readonly VITE_BASIC_AUTH_USERPOOL: string
 }
 
 interface ImportMeta {
