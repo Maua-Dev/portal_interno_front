92bfa7d819d44aad65a79db0a10cd953180a421d
Vitor Soller <vgsoller@gmail.com>
2025-09-16 02:29:48 -0300

    feat: add userpool cliente variable

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,6 +6,7 @@ interface ImportMetaEnv {
   readonly VITE_REFRESH_TOKEN_URL: string
   readonly VITE_BASIC_AUTH_USERPOOL: string
   readonly VITE_AUTH_DOMAIN: string
+  readonly VITE_USERPOOL_CLIENT_ID: string
 }
 
 interface ImportMeta {
