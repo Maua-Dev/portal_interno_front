351e96e8dc91b3ce25e5fbc2170c36bf72d18653
Enzo Chagas <enzochagas09granado@gmail.com>
2026-04-16 19:06:47 -0300

    fix: fix other´s initial forms  problems

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -20,12 +20,24 @@ export default function Home() {
   const { modalContent, changeModalContent } = useModal()
   const [notificationIsOpen, setNotificationIsOpen] = useState<boolean>(false)
 
+  const [initialized, setInitialized] = useState(false)
+
   useEffect(() => {
-    handleMember()
-    handleAllMembers()
-    changeModalContent(<Historic />)
+    const init = async () => {
+      await handleMember()
+      setInitialized(true)
+    }
+    init()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  useEffect(() => {
+    if (initialized && !isRegister && !isOnHold) {
+      handleAllMembers()
+      changeModalContent(<Historic />)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [initialized, isRegister, isOnHold])
 
   useEffect(() => {
     if (actionSuccess) {
@@ -88,7 +100,7 @@ export default function Home() {
       >
         {isOnHold ? <OnHoldModal /> : null}
         {isRegister ? <RegisterModal /> : null}
-        {!isOnHold && modalContent}
+        {!isOnHold && !isRegister && modalContent}
         <ToastContainer
           position="top-right"
           autoClose={3000}
